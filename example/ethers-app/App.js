b0539d19e3718e9ef09840bbefc210801279b731
Adrian Li <li.adrianmc@gmail.com>
2019-08-22 15:28:30 -0400

    extract blockchain logic into custom hook

--- example/ethers-app/App.js
+++ example/ethers-app/App.js
@@ -1,45 +1,8 @@
 import React, { useState, useEffect, useRef } from "react";
-import { ethers } from "ethers";
-
-import artifact from "../build/contracts/Counter.json";
+import useCounterContract from "./useCounterContract";
 
 const App = () => {
-  const contract = useRef();
-  const [count, setCount] = useState();
-
-  // function to get current count and update UI
-  const updateCount = async () => {
-    const newCount = await contract.current.getCount();
-    setCount(newCount.toString());
-  };
-
-  // function to invoke a mutating method on our contract
-  const increment = async () => {
-    const tx = await contract.current.increment();
-    await tx.wait();  // wait for mining
-    updateCount();    // update count on UI
-  };
-
-  useEffect(() => {
-    // this is only run once on component mounting
-    const setup = async () => {
-      const provider = new ethers.providers.JsonRpcProvider();
-      const network = await provider.getNetwork();
-      const contractAddress = artifact.networks[network.chainId].address;
-
-      // instantiate contract instance and assign to component ref variable
-      contract.current = new ethers.Contract(
-        contractAddress,
-        artifact.abi,
-        provider.getSigner(),
-      );
-
-      // update count on UI
-      updateCount();
-    };
-    setup();
-  }, []);
-
+  const { count, increment } = useCounterContract();
   return (
     <>
       <h1>Count: {count}</h1>
