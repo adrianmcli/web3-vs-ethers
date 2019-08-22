6ace779a8ee82be740f0d2f123e889d1e4ef8a18
Adrian Li <li.adrianmc@gmail.com>
2019-08-22 15:15:57 -0400

    complete counter app for ethers example

--- example/ethers-app/App.js
+++ example/ethers-app/App.js
@@ -1,8 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { ethers } from "ethers";
+
+import artifact from "../build/contracts/Counter.json";
 
 const App = () => {
-  const [count, setCount] = useState(-1);
-  const increment = () => setCount(count + 1);
+  const contract = useRef();
+  const [count, setCount] = useState();
+
+  // function to get current count and update UI
+  const updateCount = async () => {
+    const newCount = await contract.current.getCount();
+    setCount(newCount.toString());
+  };
+
+  // function to invoke a mutating method on our contract
+  const increment = async () => {
+    const tx = await contract.current.increment();
+    await tx.wait();  // wait for mining
+    updateCount();    // update count on UI
+  };
+
+  useEffect(() => {
+    // this is only run once on component mounting
+    const setup = async () => {
+      const provider = new ethers.providers.JsonRpcProvider();
+      const network = await provider.getNetwork();
+      const contractAddress = artifact.networks[network.chainId].address;
+
+      // instantiate contract instance and assign to component ref variable
+      contract.current = new ethers.Contract(
+        contractAddress,
+        artifact.abi,
+        provider.getSigner(),
+      );
+
+      // update count on UI
+      updateCount();
+    };
+    setup();
+  }, []);
+
   return (
     <>
       <h1>Count: {count}</h1>
