a23eb63864b419f3726c3a4f0fc10d49a058db09
Adrian Li <li.adrianmc@gmail.com>
2019-08-22 15:31:53 -0400

    remove unused variables

--- example/ethers-app/App.js
+++ example/ethers-app/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import useCounterContract from "./useCounterContract";
 
 const App = () => {
--- example/ethers-app/useCounterContract.js
+++ example/ethers-app/useCounterContract.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import { ethers } from "ethers";
 
 import artifact from "../build/contracts/Counter.json";
